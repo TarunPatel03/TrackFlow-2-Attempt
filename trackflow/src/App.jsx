18eac691854f7566104b8673678d6c67129b561d
Tarun Patel <tarun.h.patel@student.uts.edu.au>
2025-08-01 16:01:01 +1000

    phase 2 - try4

--- trackflow/src/App.jsx
+++ trackflow/src/App.jsx
@@ -5,7 +5,7 @@ import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import Home from './pages/Home';
-import Signup from './pages/SignUp';
+import Signup from './pages/Signup';
 import Login from './pages/Login';
 
 import Dashboard from './pages/Dashboard';
